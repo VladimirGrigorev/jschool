098d60861ee7b600fb0640bb0741007bdafa52b9
vladimir <grigorevvladimir1999@gmail.com>
2020-12-13 11:51:08 +0300

    Initial commit

---
+++ backend/src/test/java/com/dsr/jschool/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsr.jschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
